d33155f50380484463b31d71f08c391569b2bb6d
Diego Pizzo <>
2022-03-19 20:02:14 +0000

    create loading view

--- app/src/main/java/com/diegopizzo/whosplaying/ui/component/common/CustomView.kt
+++ app/src/main/java/com/diegopizzo/whosplaying/ui/component/common/CustomView.kt
@@ -1,12 +1,19 @@
 package com.diegopizzo.whosplaying.ui.component.common
 
 import androidx.annotation.DrawableRes
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.width
+import androidx.compose.material.CircularProgressIndicator
 import androidx.compose.material.Divider
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.Dp
 import androidx.compose.ui.unit.dp
 import coil.compose.AsyncImage
@@ -23,6 +30,8 @@ fun ComposeImage(
         model = logoUrl,
         contentDescription = null,
         placeholder = painterResource(placeholder),
+        fallback = painterResource(placeholder),
+        error = painterResource(placeholder),
         modifier = modifier
     )
 }
@@ -35,4 +44,23 @@ fun MyDivider(modifier: Modifier = Modifier, color: Color = teal700, thickness: 
 @Composable
 fun VerticalDivider(modifier: Modifier) {
     MyDivider(modifier = modifier.then(Modifier.width(1.dp)))
+}
+
+@Composable
+fun LoadingView() {
+    Box(modifier = Modifier.fillMaxSize()) {
+        Column(
+            modifier = Modifier.align(Alignment.Center),
+            horizontalAlignment = Alignment.CenterHorizontally
+        ) {
+            MediumText(stringResource(R.string.loading))
+            CircularProgressIndicator(color = Color.White)
+        }
+    }
+}
+
+@Preview
+@Composable
+fun LoadingPreview() {
+    LoadingView()
 }
