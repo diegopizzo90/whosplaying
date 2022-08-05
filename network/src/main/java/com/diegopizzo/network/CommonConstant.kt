2345bcd4678efe03c7bb8554a54d3daca5d775fc
Diego Pizzo <>
2022-08-05 21:49:57 +0100

    fix season

--- network/src/main/java/com/diegopizzo/network/CommonConstant.kt
+++ network/src/main/java/com/diegopizzo/network/CommonConstant.kt
@@ -1,8 +1,10 @@
 package com.diegopizzo.network
 
+import org.threeten.bp.LocalDate
+
 object CommonConstant {
     const val DATE_PATTERN = "EEE, d MMM yyyy"
     const val TIME_PATTERN = "HH:mm"
-    const val SEASON = "2021"
+    val SEASON = LocalDate.now().year.toString()
     const val DATE_TIME_PATTERN = "dd/MM/yyyy HH:mm"
 }
