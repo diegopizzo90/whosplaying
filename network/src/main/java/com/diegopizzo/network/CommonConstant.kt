5547572457f731f768b82b291e32c6290dbfa0c3
Diego Pizzo <>
2023-01-08 11:05:21 +0000

    fix session year

--- network/src/main/java/com/diegopizzo/network/CommonConstant.kt
+++ network/src/main/java/com/diegopizzo/network/CommonConstant.kt
@@ -5,6 +5,17 @@ import org.threeten.bp.LocalDate
 object CommonConstant {
     const val DATE_PATTERN = "EEE, d MMM yyyy"
     const val TIME_PATTERN = "HH:mm"
-    val SEASON = LocalDate.now().year.toString()
+    val SEASON = getSeasonYear()
     const val DATE_TIME_PATTERN = "dd/MM/yyyy HH:mm"
+
+
+    private fun getSeasonYear(): String {
+        val actualDate = LocalDate.now()
+        val actualMonth = actualDate.monthValue
+        return if (actualMonth == 6) {
+            actualDate.year
+        } else {
+            actualDate.year - 1
+        }.toString()
+    }
 }
