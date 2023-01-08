c54c3d2b2d33e5c03909304fb005bb330ed65599
Diego Pizzo <>
2023-01-08 11:13:07 +0000

    refactor get season function

--- network/src/main/java/com/diegopizzo/network/CommonConstant.kt
+++ network/src/main/java/com/diegopizzo/network/CommonConstant.kt
@@ -5,14 +5,14 @@ import org.threeten.bp.LocalDate
 object CommonConstant {
     const val DATE_PATTERN = "EEE, d MMM yyyy"
     const val TIME_PATTERN = "HH:mm"
-    val SEASON = getSeasonYear()
     const val DATE_TIME_PATTERN = "dd/MM/yyyy HH:mm"
-
+    private const val END_OF_SEASON = 6
+    val SEASON = getSeasonYear()
 
     private fun getSeasonYear(): String {
         val actualDate = LocalDate.now()
         val actualMonth = actualDate.monthValue
-        return if (actualMonth == 6) {
+        return if (actualMonth == END_OF_SEASON) {
             actualDate.year
         } else {
             actualDate.year - 1
